de119d792d2b8ca667d11faee16855c36e81bbe5
heesangbb <heesangbb@gmail.com>
2020-11-23 14:51:28 +0900

    express server

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const app = express();
+
+// middleware
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+// db configuration
+const MONGO_URI = process.env.MONGO_URI;
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Mongo Connection successful'))
+  .catch(err => console.log('err'));
+
+// router
+app.get('/', (req, res) => res.send('hello world!!'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server up and running on http://localhost:${PORT}`));
